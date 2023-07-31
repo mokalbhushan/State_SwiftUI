1c7c460c16eb67224b9fec775f5dec0d9f1563ce
Bhushan Mokal <bhushan.mokal@neosoftmail.com>
2023-07-31 15:42:24 +0530

    Initial Commit

---
+++ StateDemo/StateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDemoApp.swift
+//  StateDemo
+//
+//  Created by Bhushan Mokal on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct StateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
