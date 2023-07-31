24dd91e1ec5e9411e4f0a208513a0540dd7b1fa2
Bhushan Mokal <bhushanmokal02@gmail.com>
2023-07-31 16:35:22 +0530

    implemented counter

--- StateDemo/ContentView.swift
+++ StateDemo/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack(alignment: .center){
+            Button(action: { increment() },
+                   label: { Image(systemName: "plus")
+                    .frame(width: 70,height: 70)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.red,lineWidth: 2)
+                    }
+                
+            })
+            Text("\(counter)")
+                .frame(width: 100, alignment: .center)
+            Button(action: { decrement() },
+                   label: { Image(systemName: "minus")
+                    .frame(width: 70,height: 70)
+                    .clipShape(Circle())
+                    .overlay{
+                        Circle().stroke(.red,lineWidth: 2)
+                    }
+                   })
+            
         }
         .padding()
+    }
+    
+    func increment(){
+        self.counter += 1
+    }
+    
+    func decrement(){
+        self.counter -= 1
     }
 }
 
